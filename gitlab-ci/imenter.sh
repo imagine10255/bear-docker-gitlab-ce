f292a519ec3d9bdca3fd09e304800cb59876473f
imagine10255 <imagine10255@gmail.com>
2018-07-30 14:40:14 +0800

    新增 容易名稱固定, 並更改 imenter 指令

--- gitlab-ci/imenter.sh
+++ gitlab-ci/imenter.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Ports}}"
-CURRENT=$(grep './docker-compose.yml' -e 'container_name:' | awk '{ print $2 };')
-read -p "Container-Name/Id or Enter empty(default: $CURRENT): " CONTAINERID
-case $CONTAINERID in
-     *)
-        if [ "$CONTAINERID" == "" ]; then
-            docker exec -it gitlab /bin/bash
-        else
-            docker exec -it gitlab /bin/bash
-        fi
-    ;;esac
+docker exec -it imdock-gitlab-ci-gitlab /bin/bash
