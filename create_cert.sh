8ee885d4fd9630236cc54c635e2f49d2fdb8b024
imagine10255 <imagine10255@gmail.com>
2023-02-11 23:53:36 +0800

    fix ssl path

--- create_cert.sh
+++ create_cert.sh
@@ -8,11 +8,19 @@ DOMAIN="gitlab.example.com"
 EMAIL="imagine@example.com"
 MOUNT_PATH="/mnt/volume_sgp1_01/gitlab/config/ssl/letsencrypt"
 
+# $1=renew
+RENEW=$1
+
 #${DOCKER} run --rm \
 #  -v ${MOUNT_PATH}:/etc/letsencrypt \
 #  -it certbot/certbot certonly --manual --email ${EMAIL} --agree-tos
 
+#${DOCKER} run --rm \
+#  -v ${MOUNT_PATH}:/etc/letsencrypt \
+#  -v html:/html \
+#  -ti certbot/certbot certonly --email ${EMAIL} --agree-tos --webroot -w /html -d ${DOMAIN}
+
 ${DOCKER} run --rm \
   -v ${MOUNT_PATH}:/etc/letsencrypt \
-  -v ./html:/html \
-  -ti certbot/certbot certonly --email ${EMAIL} --agree-tos --webroot -w /html -d ${DOMAIN}
+  -p 80:80 \
+  -ti certbot/certbot certonly --standalone --email  ${EMAIL} --agree-tos --preferred-challenges http -d ${DOMAIN} ${RENEW}
